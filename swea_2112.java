b98ec39ac4c384cddc6c0bc9c2975b0552e20317
JinHyeonBae <compuet33@gmail.com>
2023-09-21 23:42:00 +0900

    ERROR : time out - 30 / 50

--- swea_2112.java
+++ swea_2112.java
@@ -2,125 +2,171 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 public class swea_2112 {
 
-    // 합격 기준, 보호필름 깊이, 보호필름 너비
-    static int K, D, W;
+    static boolean[] visited;
+    static int[][] cells;
+    static int[][] origin;
 
-    // cell들의 정보를 담는 배열
-    static char[][] cells;
-    static char[][] origin;
+    // 세로, 가로, 조건
+    static int D, W, K;
 
-    // 조건에 부합하는 셀인지 여부를 따짐
-    public static boolean check(int col){
-        int A = 0;
-        int B = 0;
-        for(int i = 0; i < D; i++){
-            if(cells[i][col] == 'A')
-                A++;
-            else
-                B++;
+    public static void print(int[][] m){
 
-            if(A >= K || B >= K)
-                return true;
+        System.out.println("==========================");
+        for(int i = 1; i <= D; i++){
+            for(int j = 1; j <=W; j++){
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("==========================");
+    }
+    public static boolean check(int col){ // -----> O(D)
+
+        int cnt = 0;
+        for(int r = 2; r <= D; r++){
+            if(cells[r-1][col] == cells[r][col]) {
+                cnt++;
+
+                if (cnt >= K - 1)
+                    return true;
+            }
+            else{
+                cnt = 0;
+            }
+//            // W - r
+//            if(W - r < K - 1) -> 이거 사용했더니 더 안 맞네..ㅠㅠ
+//                break;
         }
 
         return false;
     }
 
     // 셀 정보를 갱신
-    public static void update(int row, char state){
-        for(int i = 0; i < W; i++){
+    public static void update(int row, int state){ // ---> O(W)
+        for(int i = 1; i <= W; i++){
             cells[row][i] = state;
         }
     }
 
-    public static List<Integer> getCb(int cnt, int target, int[] comb, int start){
-
-        ArrayList<Integer> cbCase = new ArrayList<>();
-
-        if(target == cnt){
-            for(int i = 0; i < cnt; i++){
-                cbCase.add(comb[i]);
+    public static boolean isPassed(){ //---> O(W)
+        for (int c = 1; c <= W; c++) {
+            if (!check(c)){
+                return false;
             }
         }
-
-        for(int i = start; i < D; i++){
-            comb[cnt] = i;
-            getCb(cnt + 1, target, comb, i + 1);
-        }
-
-        return cbCase;
+        return true;
     }
 
-    public static boolean isPassed(){
-        for (int c = 0; c < W; c++) {
-            if (!check(c))
-                break;
-
-            if (c == W - 1)
-                return true;
-        }
-
-        return false;
-    }
-
-    public static void recover(){
-        for(int i = 0; i < D; i++){
+    public static void recover(int row){
+        for(int i = 1; i <= D; i++){ // O(D)
             cells[i] = origin[i].clone();
         }
     }
 
-    public static int solution(){
+    static boolean isReached = false;
+    public static void solution(int cnt, int target, int[] tc){ // 이미 순열 때문에 13! 터질 수 밖에 없다..
 
-        int passCount = 0;
+        //if(cnt > K)
+        //return;
 
-        if(isPassed())
-            return passCount;
-
-        while(true) {
-            // 변경한 걸
-
-            // 통과 못함
-            passCount++;
-            int[] cb = new int[passCount];
-            Arrays.fill(cb, -1);
-            List<Integer> result = getCb(0, passCount, cb, 0);
-
-            int size = result.size();
-            int changeCount = size - 1;
-            for(int i = 0; i < size; i++){
-                // 한 번은 A, 한 번은 B 이렇게 하고 싶다
-                for(int j = 0; j < 2; j++) {
-                    if (i <= changeCount) {
-                        update(result.get(i), 'A');
-                    } else {
-                        update(result.get(i), 'B');
-                    }
-
+        if(cnt == target){
+            int idx = 0;
+            for(int r = 1; r <= D; r++) {
+                if (visited[r]) { // row 기반으로
+                    //idx++;
+                    update(r, tc[r]);
                 }
             }
 
-            // 원상복귀
-            recover();
+            if(isPassed()){
+                isReached = true;
+                return;
+            }
 
-            if(isPassed())
-                return passCount;
+            for(int r = 1; r <= D; r++) {
+                if (visited[r]) { // row 기반으로
+                    recover(r);
+                }
+            }
+            return;
         }
+
+        for(int i = 1; i <= D; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                tc[i] = 0;
+                solution(cnt + 1, target, tc);
+
+                if(isReached)
+                    return;
+
+                tc[i] = 1;
+                solution(cnt + 1, target, tc);
+
+                if(isReached)
+                    return;
+
+                visited[i] = false;
+            }
+
+        }
+
+        return;
     }
 
     public static void init(){
-        cells = new char[D][W];
-        origin = new char[D][W];
+        cells = new int[D + 1][W + 1];
+        origin = new int[D + 1][W + 1];
+        visited = new boolean[D + 1];
+        isReached = false;
     }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine());
 
+        for(int tc = 1; tc <= T; tc++){
+            StringTokenizer stk = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(stk.nextToken());
+            W = Integer.parseInt(stk.nextToken());
+            K = Integer.parseInt(stk.nextToken());
+
+            init();
+
+            for(int r = 1; r <= D; r++) {
+                stk = new StringTokenizer(br.readLine());
+                for (int c = 1; c <= W; c++) {
+                    int value = Integer.parseInt(stk.nextToken());
+                    cells[r][c] = value;
+                    origin[r][c] = value;
+                }
+            }
+            //print(cells);
+
+            if(isPassed()){
+                System.out.println("#" + tc + " 0");
+                continue;
+            }
+
+            for(int ct = 1; ct <= D; ct++) {
+                int[] tca = new int[D + 1];
+                solution(0, ct, tca);
+
+                if(isReached) {
+                    System.out.println("#" + tc + " " + ct);
+                    break;
+                }
+            }
+            //isReached = false;
+        }
 
 
     }
---
+++ swea_2112_2.java
@@ -0,0 +1,2 @@
+package PACKAGE_NAME;public class swea_2112_2 {
+}
